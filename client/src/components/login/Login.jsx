ceb63159d9e25275f465fdf56e49a9930e78930c
Venera <vgeorgievaaaa@gmail.com>
2024-08-05 23:53:50 +0300

    implement login,register and logout logic

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,23 +1,49 @@
-export default function Login(){
-    return(
-        <div className="login-form">
-        <h1>Login</h1>
-         <form className="login-details">
-         
-           <div>
-             <label htmlFor="email">Email:</label> <br />
-             <input type="text" id="email" name="email" />
-           </div>
-     
-           <div>
-             <label htmlFor="password">Password:</label> <br />
-             <input type="password" id="password" name="password" />
-           </div>
-        
-           <div >
-             <button className="button" type="submit">Login</button>
-           </div>
-         </form>
-         </div>
-    )
+import { useContext } from "react";
+import useForm from '../hooks/useForm.js'
+import AuthContext from "../../contexts/authContext.js";
+const LoginFormKeys = {
+  Email: "email",
+  Password: "password",
+};
+
+export default function Login() {
+  const { loginSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
+    [LoginFormKeys.Email]: "",
+    [LoginFormKeys.Password]: "",
+  });
+  return (
+    <div className='login-form'>
+      <h1>Login</h1>
+      <form className='login-details' onSubmit={onSubmit}>
+        <div>
+          <label htmlFor='email'>Email:</label> <br />
+          <input
+            type='text'
+            id='email'
+            name={LoginFormKeys.Email}
+            onChange={onChange}
+            value={values[LoginFormKeys.Email]}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='password'>Password:</label> <br />
+          <input
+            type='password'
+            id='password'
+            name={LoginFormKeys.Password}
+            onChange={onChange}
+            value={values[LoginFormKeys.Password]}
+          />
+        </div>
+
+        <div>
+          <button className='button' type='submit'>
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,49 +1,64 @@
-export default function Register(){
-    return(
-        <div className="register-form">
-        <h1>Register</h1>
-         <form className="form-details">
-             <div>
-                 <label htmlFor="license-number">License Number:</label> <br />
-                 <input type="text" id="license-name" name="license-name" />
-         
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext.js";
+import useForm from '../hooks/useForm.js';
+const registerFormKeys = {
 
-            {/* { <!-- <p className={styles.errorMessage}>{errors.age}</p> -->} */}               </div>
-           <div>
-             <label htmlFor="first-name">First Name:</label> <br />
-             <input type="text" id="first-name" name="first-name" />
-     
+  Email: "email",
+  Password: "password",
+  ConfirmPassword: "confirm-password",
+};
 
-            {/* { <!-- <p className={styles.errorMessage}>{errors.age}</p> -->} */}           </div>
-           <div>
-             <label htmlFor="last-name">Last Name:</label> <br />
-             <input type="text" id="last-name" name="last-name" />
-     
+export default function Register() {
+  const { registerSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
 
-            {/* { <!-- <p className={styles.errorMessage}>{errors.age}</p> -->} */}           </div>
-           <div>
-             <label htmlFor="email">Email:</label> <br />
-             <input type="text" id="email" name="email" />
-     
-            {/* { <!-- <p className={styles.errorMessage}>{errors.age}</p> -->} */}
-           </div>
-     
-           <div>
-             <label htmlFor="password">Password:</label> <br />
-             <input type="password" id="password" name="password" />
-           </div>
-           <div>
-             <label htmlFor="confirm-password">Confirm password:</label> <br />
-             <input
-               type="confirm-password"
-               id="confirm-password"
-               name="confirm-password"
-             />
-           </div>
-           <div >
-             <button className="button" type="submit">Register</button>
-           </div>
-         </form>
-         </div>
-    )
+    [registerFormKeys.Email]: "",
+    [registerFormKeys.Password]: "",
+    [registerFormKeys.ConfirmPassword]: "",
+  });
+  return (
+    <div className='register-form'>
+      <h1>Register</h1>
+      <form className='form-details' onSubmit={onSubmit}>
+   
+        <div>
+          <label htmlFor='email'>Email:</label> <br />
+          <input
+            type='text'
+            id='email'
+            name='email'
+            onChange={onChange}
+            values={values[registerFormKeys.Email]}
+          />
+          {/* { <!-- <p className={styles.errorMessage}>{errors.age}</p> -->} */}
+        </div>
+
+        <div>
+          <label htmlFor='password'>Password:</label> <br />
+          <input
+            type='password'
+            id='password'
+            name='password'
+            onChange={onChange}
+            values={values[registerFormKeys.Password]}
+          />
+        </div>
+        <div>
+          <label htmlFor='confirm-password'>Confirm password:</label> <br />
+          <input
+            type='-password'
+            id='confirm-password'
+            name='confirm-password'
+            onChange={onChange}
+            values={values[registerFormKeys.ConfirmPassword]}
+          />
+        </div>
+        <div>
+          <button className='button' type='submit' >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
