7ae6cccd7e5e5e91784a0596f3edef39e9351f84
Venera <vgeorgievaaaa@gmail.com>
2024-08-13 12:53:18 +0300

    add search bar and add searchItem logic

--- client/src/components/blog/Blog.jsx
+++ client/src/components/blog/Blog.jsx
@@ -1,46 +1,62 @@
-import { useEffect, useState } from "react"
-import * as postService from "../../services/postService.js"
+import { useEffect, useState } from "react";
+import * as postService from "../../services/postService.js";
 import BlogPostItem from "./blog-post-item/BlogPostItem.jsx";
+import { Link } from "react-router-dom";
+import SearchItem from "./blog-post-item/SearchItem.jsx";
 
-export default function Blog(){
-  const[posts,setPosts] = useState([]);
+export default function Blog() {
+  const [posts, setPosts] = useState([]);
 
-  useEffect(()=>{
-    postService.getAll()
-    .then(result => setPosts(result));
-    
-},[]);
+  useEffect(() => {
+    postService.getAll().then((result) => setPosts(result));
+  }, []);
 
+  const [searchItem, setSearchItem] = useState("");
+  console.log(posts);
+  
 
-    return(
-        <div className="blog-container">
-                <div className="blog-title">
+  return (
+    <div className='blog-container'>
+      <div className='blog-title'>
         <h1>Blog</h1>
       </div>
-  
-      <div className="wrap">
-        <div className="search">
+
+      <div className='search'>
           <input
-            type="text"
-            className="search-term"
-            placeholder="What are you looking for?"
+            type='text'
+            className='search-term'
+            placeholder='What are you looking for?'
+            onChange={(e) => {
+              setSearchItem(e.target.value);
+            }}
           />
-          <button type="submit" className="search-button">Search</button>
         </div>
-      </div>
+        
+      <div className='wrap'>
   
-      <div className="all-posts">
-         {posts.map(posted => (
-        <BlogPostItem key={posted._id} {...posted} />
-     ))}
 
-     {posts.length === 0 && (
-                <h3 className="no-articles">No articles yet</h3>
- ) }
-      </div>
-    
-
+        <div className='all-posts'>
+        {posts.filter((value) => {
+              if (searchItem == "") {
+                return value;
+              } else if (
+                value.author.toLowerCase().includes(searchItem.toLowerCase())
+              ) {
+                return value;
+              }
+            }).map((posted) => (
+          <SearchItem key={posted._id} {...posted} />
+        ))}
         </div>
+      </div>
 
-    )
+      <div className='all-posts'>
+        {/* {posts.map((posted) => (
+          <BlogPostItem key={posted._id} {...posted} />
+        ))} */}
+
+        {posts.length === 0 && <h3 className='no-articles'>No articles yet</h3>}
+      </div>
+    </div>
+  );
 }
---
+++ client/src/components/blog/blog-post-item/SearchItem.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export default function SearchItem({
+    _id,
+    author,
+    title,
+    imageUrl,
+    
+
+}){
+    return(
+
+        <>
+        <div className="blog-posts">
+       <article className="post">
+         <h2>Article written by: {author}</h2>
+         <h1>{title}</h1>
+         <img src={imageUrl} alt="" />
+ 
+         <Link to={`/posts/${_id}`}>Details</Link>
+       </article>
+     </div>
+     </>
+    )
+
+}
